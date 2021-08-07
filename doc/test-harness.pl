9dced247615bd2fc9572bfa652458829bf59f740
gpaulissen <gert.jan.paulissen@gmail.com>
2021-08-07 13:21:12 +0200

    Test harness added

--- doc/test-harness.pl
+++ doc/test-harness.pl
@@ -1,46 +1,84 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+use File::Spec::Functions 'catfile';
+use File::Basename;
+
+my $CMAKE_CURRENT_SOURCE_DIR = dirname($0);
+my $CMAKE_CURRENT_BINARY_DIR = shift @ARGV;
+my $factorial = catfile($CMAKE_CURRENT_BINARY_DIR, 'factorial');
+my $diff = 'diff';
+
+sub execute ($);
+sub check_status ($$);
+sub main();
+
+main();
+
+sub main() {
+    foreach my $argv (@ARGV) {
+        eval "$argv";
+        die $@
+            if $@;
+    }
+}
+
 sub test1() {
-# $(FACTORIAL) 1 2 3 4 5 > $@.log
-# $(DIFF) $@.log $(doc_srcdir)/$@.ref
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial 1 2 3 4 5 > test1.log");
+    execute("$diff test1.log $CMAKE_CURRENT_SOURCE_DIR/test1.ref");
 }
 
 sub test2() {
-# $(FACTORIAL) -#t 3 2 | @PERL@ $(DBUGRPT) > $@.log
-# $(DIFF) $@.log $(doc_srcdir)/$@.ref
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial -#t 3 2 | perl $CMAKE_CURRENT_SOURCE_DIR/../src/prog/dbugrpt > test2.log");
+    execute("$diff test2.log $CMAKE_CURRENT_SOURCE_DIR/test2.ref");
 }
 
 sub test3() {
-# $(FACTORIAL) -#d,t 3 | @PERL@ $(DBUGRPT) > $@.log
-# $(DIFF) $@.log $(doc_srcdir)/$@.ref
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial -#d,t 3 | perl $CMAKE_CURRENT_SOURCE_DIR/../src/prog/dbugrpt > test3.log");
+    execute("$diff test3.log $CMAKE_CURRENT_SOURCE_DIR/test3.ref");
 }
 
 sub test4() {
-# $(FACTORIAL) -#d 4 | @PERL@ $(DBUGRPT) -d result > $@.log
-# $(DIFF) $@.log $(doc_srcdir)/$@.ref
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial -#d 4 | perl $CMAKE_CURRENT_SOURCE_DIR/../src/prog/dbugrpt -d result > test4.log");
+    execute("$diff test4.log $CMAKE_CURRENT_SOURCE_DIR/test4.ref");
 }
 
 sub test5() {
-## GJP 19-3-2003 Ignore changes due to differences between source and build locations.
-# $(FACTORIAL) -#d 3 | @PERL@ $(DBUGRPT) -t factorial -FL | sed -e 's!$(top_srcdir)/doc/!!g' > $@.log
-# $(DIFF) $@.log $(doc_srcdir)/$@.ref
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial -#d 3 | perl $CMAKE_CURRENT_SOURCE_DIR/../src/prog/dbugrpt -t factorial -FL | perl -ne 's!$CMAKE_CURRENT_SOURCE_DIR/!!; print' > test5.log");
+    execute("$diff test5.log $CMAKE_CURRENT_SOURCE_DIR/test5.ref");
 }
 
 sub test6() {
-# $(FACTORIAL) -#t,D=1000,g 3 | @PERL@ $(DBUGRPT) > $@.log
-# @PERL@ $(srcdir)/diffrex.pl $(srcdir)/test6.ref $@.log
-# @echo : > $@
-# @chmod +x $@
+    execute("$factorial -#t,D=1000,g 3 | perl $CMAKE_CURRENT_SOURCE_DIR/../src/prog/dbugrpt > test6.log");
+    execute("perl $CMAKE_CURRENT_SOURCE_DIR/diffrex.pl $CMAKE_CURRENT_SOURCE_DIR/test6.ref test6.log");
 }
 
-eval "$ARGV[0]";
+sub execute ($) {
+    my ($cmd) = @_;
+
+    eval {
+        system($cmd);
+    };
+
+    my $process = $cmd;
+    
+    die "$process\n$@" if $@;
+    check_status($process, $?);
+}
+
+sub check_status ($$) {
+    my ($process, $status) = @_;
+
+    if (defined($status)) {
+        if ($status == -1) {
+            die "$process\nFailed to execute: $!";
+        }
+        elsif ($status & 127) {
+            die sprintf("$process\nChild died with signal %d, %s coredump", ($status & 127),  ($status & 128) ? 'with' : 'without');
+        }
+        elsif (($status >> 8) != 0) {
+            die sprintf("$process\nChild exited with value %d", $status >> 8);
+        }
+    }
+}
