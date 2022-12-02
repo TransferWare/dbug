70654cbb65d9a874845c4f812e85d9e46338eda4
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-12-02 14:15:33 +0100

    Update uninstall.sql

--- src/sql/uninstall.sql
+++ src/sql/uninstall.sql
@@ -1,10 +1,10 @@
-/* perl generate_ddl.pl (version 2021-08-27) --nodynamic-sql --force-view --skip-install-sql --nostrip-source-schema */
+/* perl generate_ddl.pl (version 2022-09-28) --nodynamic-sql --force-view --group-constraints --skip-install-sql --source-schema=EPCAPP --strip-source-schema */
 
 /*
--- JDBC url            : jdbc:oracle:thin:ORACLE_TOOLS@//localhost:1521/orcl
+-- JDBC url            : jdbc:oracle:thin:EPCAPP@//localhost:1521/orcl
 -- source schema       : 
 -- source database link: 
--- target schema       : ORACLE_TOOLS
+-- target schema       : EPCAPP
 -- target database link: 
 -- object type         : 
 -- object names include: 1
@@ -27,104 +27,123 @@ STD_OBJECTS,
 */
 -- pkg_ddl_util v5
 call dbms_application_info.set_module('uninstall.sql', null);
-/* SQL statement 1 (ALTER;ORACLE_TOOLS;CONSTRAINT;STD_OBJECTS_CHK4;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
+/* SQL statement 1 (REVOKE;;OBJECT_GRANT;;EPCAPP;PACKAGE_SPEC;DBUG;;PUBLIC;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 1');
-ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK4;
+REVOKE EXECUTE ON "DBUG" FROM "PUBLIC";
 
-/* SQL statement 2 (ALTER;ORACLE_TOOLS;CONSTRAINT;STD_OBJECTS_CHK3;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
+/* SQL statement 2 (ALTER;EPCAPP;CONSTRAINT;STD_OBJECTS_CHK1;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
 call dbms_application_info.set_action('SQL statement 2');
-ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK3;
+ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK1;
 
-/* SQL statement 3 (ALTER;ORACLE_TOOLS;CONSTRAINT;STD_OBJECTS_CHK2;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
+/* SQL statement 3 (ALTER;EPCAPP;CONSTRAINT;STD_OBJECTS_CHK2;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
 call dbms_application_info.set_action('SQL statement 3');
 ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK2;
 
-/* SQL statement 4 (ALTER;ORACLE_TOOLS;CONSTRAINT;STD_OBJECTS_CHK1;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
+/* SQL statement 4 (ALTER;EPCAPP;CONSTRAINT;STD_OBJECTS_CHK3;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
 call dbms_application_info.set_action('SQL statement 4');
-ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK1;
+ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK3;
 
-/* SQL statement 5 (ALTER;ORACLE_TOOLS;CONSTRAINT;STD_OBJECTS_PK;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
+/* SQL statement 5 (ALTER;EPCAPP;CONSTRAINT;STD_OBJECTS_CHK4;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
 call dbms_application_info.set_action('SQL statement 5');
+ALTER TABLE "STD_OBJECTS" DROP CONSTRAINT STD_OBJECTS_CHK4;
+
+/* SQL statement 6 (ALTER;EPCAPP;CONSTRAINT;STD_OBJECTS_PK;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
+call dbms_application_info.set_action('SQL statement 6');
 ALTER TABLE "STD_OBJECTS" DROP PRIMARY KEY KEEP INDEX;
 
-/* SQL statement 6 (DROP;ORACLE_TOOLS;INDEX;STD_OBJECTS_PK;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;2) */
-call dbms_application_info.set_action('SQL statement 6');
-DROP INDEX STD_OBJECTS_PK;
-
-/* SQL statement 7 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG;;;;;;;;2) */
+/* SQL statement 7 (DROP;EPCAPP;INDEX;STD_OBJECTS_PK;EPCAPP;TABLE;STD_OBJECTS;;;;;2) */
 call dbms_application_info.set_action('SQL statement 7');
-DROP PACKAGE BODY DBUG;
+DROP INDEX EPCAPP.STD_OBJECTS_PK;
 
-/* SQL statement 8 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG_DBMS_APPLICATION_INFO;;;;;;;;2) */
+/* SQL statement 8 (DROP;EPCAPP;PACKAGE_BODY;DBUG;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 8');
-DROP PACKAGE BODY DBUG_DBMS_APPLICATION_INFO;
+DROP PACKAGE BODY EPCAPP.DBUG;
 
-/* SQL statement 9 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG_DBMS_OUTPUT;;;;;;;;2) */
+/* SQL statement 9 (DROP;EPCAPP;PACKAGE_BODY;DBUG_DBMS_APPLICATION_INFO;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 9');
-DROP PACKAGE BODY DBUG_DBMS_OUTPUT;
+DROP PACKAGE BODY EPCAPP.DBUG_DBMS_APPLICATION_INFO;
 
-/* SQL statement 10 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG_LOG4PLSQL;;;;;;;;2) */
+/* SQL statement 10 (DROP;EPCAPP;PACKAGE_BODY;DBUG_DBMS_OUTPUT;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 10');
-DROP PACKAGE BODY DBUG_LOG4PLSQL;
+DROP PACKAGE BODY EPCAPP.DBUG_DBMS_OUTPUT;
 
-/* SQL statement 11 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG_PROFILER;;;;;;;;2) */
+/* SQL statement 11 (DROP;EPCAPP;PACKAGE_BODY;DBUG_LOG4PLSQL;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 11');
-DROP PACKAGE BODY DBUG_PROFILER;
+DROP PACKAGE BODY EPCAPP.DBUG_LOG4PLSQL;
 
-/* SQL statement 12 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DBUG_TRIGGER;;;;;;;;2) */
+/* SQL statement 12 (DROP;EPCAPP;PACKAGE_BODY;DBUG_PROFILER;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 12');
-DROP PACKAGE BODY DBUG_TRIGGER;
+DROP PACKAGE BODY EPCAPP.DBUG_PROFILER;
 
-/* SQL statement 13 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG_DBMS_APPLICATION_INFO;;;;;;;;2) */
+/* SQL statement 13 (DROP;EPCAPP;PACKAGE_BODY;DBUG_TRIGGER;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 13');
-DROP PACKAGE DBUG_DBMS_APPLICATION_INFO;
+DROP PACKAGE BODY EPCAPP.DBUG_TRIGGER;
 
-/* SQL statement 14 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG_DBMS_OUTPUT;;;;;;;;2) */
+/* SQL statement 14 (DROP;EPCAPP;PACKAGE_SPEC;DBUG_DBMS_APPLICATION_INFO;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 14');
-DROP PACKAGE DBUG_DBMS_OUTPUT;
+DROP PACKAGE EPCAPP.DBUG_DBMS_APPLICATION_INFO;
 
-/* SQL statement 15 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG_LOG4PLSQL;;;;;;;;2) */
+/* SQL statement 15 (DROP;EPCAPP;PACKAGE_SPEC;DBUG_DBMS_OUTPUT;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 15');
-DROP PACKAGE DBUG_LOG4PLSQL;
+DROP PACKAGE EPCAPP.DBUG_DBMS_OUTPUT;
 
-/* SQL statement 16 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG_PROFILER;;;;;;;;2) */
+/* SQL statement 16 (DROP;EPCAPP;PACKAGE_SPEC;DBUG_LOG4PLSQL;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 16');
-DROP PACKAGE DBUG_PROFILER;
+DROP PACKAGE EPCAPP.DBUG_LOG4PLSQL;
 
-/* SQL statement 17 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG_TRIGGER;;;;;;;;2) */
+/* SQL statement 17 (DROP;EPCAPP;PACKAGE_SPEC;DBUG_PROFILER;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 17');
-DROP PACKAGE DBUG_TRIGGER;
+DROP PACKAGE EPCAPP.DBUG_PROFILER;
 
-/* SQL statement 18 (DROP;ORACLE_TOOLS;TYPE_BODY;DBUG_OBJ_T;;;;;;;;2) */
+/* SQL statement 18 (DROP;EPCAPP;PACKAGE_SPEC;DBUG_TRIGGER;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 18');
-DROP TYPE BODY DBUG_OBJ_T;
+DROP PACKAGE EPCAPP.DBUG_TRIGGER;
 
-/* SQL statement 19 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DBUG;;;;;;;;2) */
+/* SQL statement 19 (DROP;EPCAPP;TYPE_BODY;DBUG_OBJ_T;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 19');
-DROP PACKAGE DBUG;
+DROP TYPE BODY EPCAPP.DBUG_OBJ_T;
 
-/* SQL statement 20 (DROP;ORACLE_TOOLS;TYPE_BODY;DBUG_LOG4PLSQL_OBJ_T;;;;;;;;2) */
+/* SQL statement 20 (DROP;EPCAPP;PACKAGE_SPEC;DBUG;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 20');
-DROP TYPE BODY DBUG_LOG4PLSQL_OBJ_T;
+DROP PACKAGE EPCAPP.DBUG;
 
-/* SQL statement 22 (DROP;ORACLE_TOOLS;TABLE;STD_OBJECTS;;;;;;;;2) */
+/* SQL statement 21 (DROP;EPCAPP;TYPE_BODY;DBUG_LOG4PLSQL_OBJ_T;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 21');
+DROP TYPE BODY EPCAPP.DBUG_LOG4PLSQL_OBJ_T;
+
+/* SQL statement 22 (DROP;EPCAPP;TABLE;STD_OBJECTS;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 22');
-DROP TABLE STD_OBJECTS PURGE;
+DROP TABLE EPCAPP.STD_OBJECTS PURGE;
 
-/* SQL statement 23 (DROP;ORACLE_TOOLS;TYPE_SPEC;DBUG_OBJ_T;;;;;;;;2) */
+/* SQL statement 23 (DROP;EPCAPP;TYPE_BODY;STD_OBJECT;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 23');
-DROP TYPE DBUG_OBJ_T;
+DROP TYPE BODY EPCAPP.STD_OBJECT;
 
-/* SQL statement 24 (DROP;ORACLE_TOOLS;TYPE_SPEC;DBUG_CALL_TAB_T;;;;;;;;2) */
+/* SQL statement 24 (DROP;EPCAPP;TYPE_SPEC;DBUG_OBJ_T;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 24');
-DROP TYPE DBUG_CALL_TAB_T;
+DROP TYPE EPCAPP.DBUG_OBJ_T;
 
-/* SQL statement 25 (DROP;ORACLE_TOOLS;TYPE_SPEC;DBUG_CALL_OBJ_T;;;;;;;;2) */
+/* SQL statement 25 (DROP;EPCAPP;TYPE_SPEC;DBUG_CALL_TAB_T;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 25');
-DROP TYPE DBUG_CALL_OBJ_T;
+DROP TYPE EPCAPP.DBUG_CALL_TAB_T;
 
-/* SQL statement 26 (DROP;ORACLE_TOOLS;TYPE_SPEC;DBUG_LOG4PLSQL_OBJ_T;;;;;;;;2) */
+/* SQL statement 26 (DROP;EPCAPP;TYPE_SPEC;DBUG_CALL_OBJ_T;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 26');
-DROP TYPE DBUG_LOG4PLSQL_OBJ_T;
+DROP TYPE EPCAPP.DBUG_CALL_OBJ_T;
 
-drop type std_object force;
+/* SQL statement 27 (DROP;EPCAPP;TYPE_SPEC;DBUG_LOG4PLSQL_OBJ_T;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 27');
+DROP TYPE EPCAPP.DBUG_LOG4PLSQL_OBJ_T;
+
+/* SQL statement 28 (DROP;EPCAPP;TYPE_SPEC;STD_OBJECT;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 28');
+DROP TYPE EPCAPP.STD_OBJECT;
+
+/* SQL statement 29 (DROP;PUBLIC;SYNONYM;DBUG;EPCAPP;PACKAGE_SPEC;DBUG;;;;;2) */
+call dbms_application_info.set_action('SQL statement 29');
+DROP PUBLIC SYNONYM "DBUG";
+
+/* SQL statement 30 (DROP;PUBLIC;SYNONYM;DBUG_TRIGGER;EPCAPP;PACKAGE_SPEC;DBUG_TRIGGER;;;;;2) */
+call dbms_application_info.set_action('SQL statement 30');
+DROP PUBLIC SYNONYM "DBUG_TRIGGER";
+
