8707706bb9b92d6a276a05801e008fded6d1adf6
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2025-10-23 09:57:07 +0200

    Splitting install scripts so they can be used by the PATO.

---
+++ src/sql/install-harness.sql
@@ -0,0 +1,10 @@
+whenever oserror exit failure
+whenever sqlerror exit failure
+set define off sqlblanklines on
+ALTER SESSION SET PLSQL_WARNINGS = 'ENABLE:ALL';
+
+set define '^'
+
+@@ ^1
+
+undefine 1
